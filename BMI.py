c3bcbe21dd10bda810177d093d4af2af08323313
Abhijithnothing <a00931668@gmail.com>
2025-11-11 10:56:45 +0530

    save

--- BMI.py
+++ BMI.py
@@ -1,11 +1,16 @@
 import sys
-
-if len(sys.argv) != 3:
-    print("Usage: python BMI.py <weight_in_kg> <height_in_meters>")
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    weight = sys.argv[1]
+    height = sys.argv[2]
 else:
-    weight = float(sys.argv[1])
-    height = float(sys.argv[2])
-
-    bmi = weight / (height ** 2)
-
-    print(f"BMI: {bmi:.2f}")
+    script_name = sys.argv[0]
+    weight = 60
+    height = 170
+print("Invalid input using default values")
+weight = float(weight)
+height = float(height) / 100
+bmi = weight / (height*height)
+print("Weight", weight, "kg")
+print("Height", height, "m")
+print("BMI", bmi)
